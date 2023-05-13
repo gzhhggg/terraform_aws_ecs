01a371204456c4fca815a70d0af9f1e171ac9e4a
gzhhggg <keiichimonoo@gmail.com>
2023-05-13 16:52:49 +0900

    ECRとECSの作成からやり直し

--- ecr.tf
+++
@@ -1,9 +0,0 @@
-
-#ECRリポジトリの作成
-resource "aws_ecr_repository" "default" {
-  name                 = local.repository
-  image_tag_mutability = "MUTABLE"
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
